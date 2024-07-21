a599ccfa9b092b5f44860775e81aff93611fb030
LucasMalagoli <l.malagoli@seazone.com.br>
2024-07-21 20:54:07 -0300

    Add hover effect to Folder component

--- src/components/MainContainer/Folder.tsx
+++ src/components/MainContainer/Folder.tsx
@@ -1,16 +1,43 @@
 import { Card, HStack, Icon, Text } from "@chakra-ui/react";
-import { BsFolder } from "react-icons/bs";
+import { useState } from "react";
+import { BsFolder, BsFolderFill } from "react-icons/bs";
 
 interface Props {
   folderName: string;
 }
 
 const Folder = ({ folderName }: Props) => {
+  const [hover, setHover] = useState(false);
+
   return (
-    <Card bg={"#f0f4f9"} display={"inline"} padding={3}>
+    <Card
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      _hover={{
+        backgroundColor: "#dfe3e7",
+        transition: "backgroundColor, .2s",
+      }}
+      bg={"#f0f4f9"}
+      display={"inline"}
+      padding={3}
+    >
       <HStack>
-        <Icon as={BsFolder} boxSize={6} />
-        <Text>{folderName}</Text>
+        <Icon
+          as={BsFolder}
+          boxSize={6}
+          position={"absolute"}
+          opacity={hover ? 0 : 1}
+          transition={"opacity .2s"}
+        />
+        <Icon
+          as={BsFolderFill}
+          boxSize={6}
+          opacity={hover ? 1 : 0}
+          transition={"opacity .2s"}
+        />
+        <Text fontWeight={500} cursor={"default"}>
+          {folderName}
+        </Text>
       </HStack>
     </Card>
   );
