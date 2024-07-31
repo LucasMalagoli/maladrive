d2ad79958042245756219ac7b9f988cd298a84b4
LucasMalagoli <l.malagoli@seazone.com.br>
2024-07-30 21:52:23 -0300

    Created search bar component

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,7 +1,28 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { SlMagnifier } from "react-icons/sl";
 
 const SearchBar = () => {
-  return <Box flex={1}>SearchBar</Box>;
+  return (
+    <Box flex={1}>
+      <InputGroup>
+        <InputLeftElement>
+          <SlMagnifier />
+        </InputLeftElement>
+        <Input
+          borderRadius={10}
+          borderColor={"#ccc"}
+          variant="filled"
+          placeholder={"WIP"}
+          htmlSize={70}
+          width="auto"
+          _focusVisible={{
+            outline: "none",
+            borderColor: "#000",
+          }}
+        />
+      </InputGroup>
+    </Box>
+  );
 };
 
 export default SearchBar;
